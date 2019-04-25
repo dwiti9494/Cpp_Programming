fbfad9e3d332528c53fb4435ca5eade125a25d4f
Dwiti Patel <49846497+dwiti9494@users.noreply.github.com>
2019-04-25 14:02:59 -0700

    Add files via upload

---
+++ example8.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <limits>
+
+using namespace std;
+
+int main()
+{
+    int value = 54656;
+    cout << value << endl;
+
+    cout <<"Max int valur: " << INT_MAX << endl;
+    cout <<"Min int valur: " << INT_MIN << endl;
+
+    long int lValue = 234567898;
+    cout << lValue << endl;
+
+    short int sValue = 23345;
+    cout << sValue << endl;
+
+
+    return 0;
+}
