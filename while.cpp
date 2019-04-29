ccacfba90fad50bfcb873e050aac4ddcc5244097
Dwiti Patel <49846497+dwiti9494@users.noreply.github.com>
2019-04-28 19:10:03 -0700

    Add files via upload

---
+++ while.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 0;
+    while(i < 5)
+    {
+       cout << "Hello " << i <<  endl;
+       i++; // increment i by 1
+    }
+    cout << "Programming quitting." << endl;
+    return 0;
+}
