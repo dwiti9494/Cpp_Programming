73a072e49173bfb88ce1e501e5d7441c79792b55
Dwiti Patel <49846497+dwiti9494@users.noreply.github.com>
2019-04-30 16:47:25 -0700

    Add files via upload

---
+++ functions.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+void showMenu()
+{
+    cout << "1. Search" << endl;
+    cout << "2. View record" << endl;
+    cout << "3. Quit" << endl;
+}
+void processSelection()
+{
+    cout << " Enter selection: " << endl;
+
+    int input;
+    cin >> input;
+
+    switch (input)
+    {
+    case 1:
+        cout << "Searching.." << endl;
+        break;
+    case 2:
+        cout << "Viewing.." << endl;
+        break;
+    case 3:
+        cout << "Quitting.." << endl;
+        break;
+    default:
+        cout << "Please select an item from the menu." << endl;
+    }
+}
+int main()
+{
+    showMenu();
+    processSelection();
+
+    return 0;
+}
