80902409720823d491c84bd47eb1c9048f5d0007
Dwiti Patel <49846497+dwiti9494@users.noreply.github.com>
2019-05-01 13:45:52 -0700

    Add files via upload

---
+++ headersPrototype.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "utils.h"
+using namespace std;
+
+
+int main()
+{
+    doSomething();
+    doSomething();
+
+    return 0;
+}
+
+void doSomething()
+{
+    cout << "Hello" << endl;
+}
