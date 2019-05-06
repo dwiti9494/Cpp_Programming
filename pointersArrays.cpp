7bb0d6c154f684208801b1e06450fd99bd89bb88
Dwiti Patel <49846497+dwiti9494@users.noreply.github.com>
2019-05-06 14:33:36 -0700

    Add files via upload

---
+++ pointersArrays.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string texts[] = {"one", "two", "three"};
+    string *pTexts = texts;
+
+    for (int i = 0; i < sizeof(texts) / sizeof(string); i++)
+    {
+        cout << pTexts[i] << " " << flush;
+    }
+    cout << endl;
+
+    for (int i = 0; i < sizeof(texts) / sizeof(string); i++, pTexts++)
+    {
+        cout << *pTexts << " " << flush;
+    }
+    cout << endl;
+
+    string *pElement = &texts[0];
+    string *pEnd = &texts[2];
+
+    while (true)
+    {
+        cout << *pElement << " " << flush;
+
+        if (pElement == pEnd)
+        {
+            break;
+        }
+        pElement++;
+    }
+
+    return 0;
+}
