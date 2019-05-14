de48eb75011a13623c857012acc1675cd6b71cb8
Dwiti Patel <49846497+dwiti9494@users.noreply.github.com>
2019-05-13 17:19:39 -0700

    Add files via upload

---
+++ vector.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void printVector(vector<int> b)
+{
+    cout << " | ";
+    for(int i = 0; i < b.size(); i++)
+    {
+        cout << b[i] << " | ";
+    }
+}
+int main()
+{
+    vector<int> v(3);
+
+    v[0] = 10;
+    v[1] = 22;
+    v[2] = 5;
+
+    v.push_back(7);
+
+    cout << v.front() << endl;
+    cout << v.back() << endl;
+    cout << v.at(2) << endl;
+
+    printVector(v);
+
+    return 0;
+}
